feb83a658ece62286918a65602f6219883e4a9d9
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-19 22:53:59 +0530

    Time: 1887 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -1,7 +1,6 @@
-
-select p.product_id, p.product_name
+select p.product_id,p.product_name
 from product as p
 join sales as s on p.product_id = s.product_id
 group by p.product_id
-having min(s.sale_date)>='2019-01-01' 
-and max(s.sale_date)<='2019-03-31'
+having min(sale_date)>='2019-01-01'
+and max(sale_date)<='2019-03-31';
