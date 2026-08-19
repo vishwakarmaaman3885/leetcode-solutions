2e85b0e74100ae9768f5e1f829e3758085314759
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-19 22:09:05 +0530

    Time: 1775 ms (8.50%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,3 +1,6 @@
-select p.product_name as product_name,s.year as year,s.price as price
+with ct as
+(select p.product_name as product_name,s.year as year,s.price as price
 from sales as s 
-join product as p on s.product_id = p.product_id
+join product as p on s.product_id = p.product_id)
+select product_name,year,price
+from ct;
