7c68363a89a8dc4a491e353d129c808eeffee2cb
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-19 22:27:20 +0530

    Time: 638 ms (23.19%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,5 +1,7 @@
-select p.project_id as project_id, round(avg(experience_years),2) as average_years
+with cte as 
+(select p.project_id as project_id, e.experience_years as experience_years
 from project as p
-join employee as e on p.employee_id = e.employee_id
-group by project_id
-
+join employee as e on p.employee_id = e.employee_id)
+select project_id,round(avg(experience_years),2) as average_years
+from cte
+group by project_id;
