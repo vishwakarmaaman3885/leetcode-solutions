460a116059d8a7892fadd6b4b381dfce943c31af
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-19 21:28:02 +0530

    Time: 304 ms (32.87%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,4 +1,5 @@
-select id, movie,description,rating
+select *
+from (select id, movie,description,rating
 from cinema
 where description != 'boring' and id % 2=1
-order by rating desc;
+order by rating desc) as a;
