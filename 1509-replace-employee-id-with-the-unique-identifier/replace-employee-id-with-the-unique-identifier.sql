824d69acb79b94c15bbffff26a9043be2029bead
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-20 11:41:17 +0530

    Time: 1424 ms (34.97%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,8 +1,5 @@
 ### Aman
 
-with cte as
-(select e.unique_id, em.name
+select e.unique_id, em.name
 from employees as em
-left join employeeuni as e on em.id = e.id)
-select unique_id,name
-from cte
+left join employeeuni as e on em.id = e.id;
