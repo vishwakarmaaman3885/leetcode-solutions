1c736979c12779cca68565bdbf42f62394bc7b79
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-20 11:27:18 +0530

    Time: 1257 ms (5.26%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,4 +1,4 @@
-select p.product_name, sum(o.unit) as unit
+select p.product_name,sum(o.unit) as unit
 from products as p
 join orders as o on p.product_id = o.product_id
 where o.order_date between '2020-02-01' and '2020-02-29'
