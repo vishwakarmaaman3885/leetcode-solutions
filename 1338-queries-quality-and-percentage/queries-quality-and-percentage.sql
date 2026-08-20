1de90d0404d396ce86b8df10553d1bb6bfa3f53e
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-20 08:49:07 +0530

    Time: 406 ms (43.18%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -2,6 +2,6 @@ with cte as (select query_name, result,position,rating
 from queries)
 select query_name, 
 round(avg(rating/position),2) as quality,
-round(count(case when rating<3 then result end)*100.00/count(position),2) as poor_query_percentage
+round(count(case when rating<3 then result end)*100.00/count(*),2) as poor_query_percentage
 from cte
 group by query_name;
