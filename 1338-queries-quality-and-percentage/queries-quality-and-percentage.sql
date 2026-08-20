19874f8b51b4d001b03417113c0ca67685df45d7
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-20 08:46:52 +0530

    Time: 472 ms (16.33%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,6 +1,7 @@
-select query_name,
-round(avg(rating/position),2) as quality, 
-round(count(CASE when rating<3 then result  end)*100.00/count(rating),2) as
-poor_query_percentage
-from queries
+with cte as (select query_name, result,position,rating
+from queries)
+select query_name, 
+round(avg(rating/position),2) as quality,
+round(count(case when rating<3 then result end)*100.00/count(position),2) as poor_query_percentage
+from cte
 group by query_name;
