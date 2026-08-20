becf1eeb7c41a761304a42dcad339ed9315ace92
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-20 11:32:12 +0530

    Time: 1006 ms (14.96%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,6 +1,9 @@
-select p.product_name,sum(o.unit) as unit
-from products as p
+with cte as 
+(select p.product_name, o.unit
+from products as p 
 join orders as o on p.product_id = o.product_id
-where o.order_date between '2020-02-01' and '2020-02-29'
-group by p.product_name
-having sum(o.unit)>=100;
+where order_date between '2020-02-01' and '2020-02-29')
+select product_name, sum(unit) as unit
+from cte
+group by product_name
+having sum(unit)>=100;
