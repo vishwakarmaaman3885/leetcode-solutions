4f721e7b79d86f43d3b01cdd8f75a8f6c7b7b45a
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-21 10:57:37 +0530

    Time: 257 ms (69.53%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,6 @@
+select machine_id,
+round(avg(next-timestamp),3) as processing_time
+from (select *,
+Lead(timestamp) over(partition by machine_id,process_id order by timestamp) as next
+from activity) as a
+group by machine_id
