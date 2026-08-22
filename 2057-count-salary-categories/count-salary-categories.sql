77f294ade116e3a5f5ebd1ddc76001b9c63bfad2
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-22 09:13:04 +0530

    Time: 1468 ms (89.96%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,11 +1,14 @@
 select 'Low Salary' as category
-,count(case when income<20000 then 1 end) as accounts_count
-from accounts 
+,count(*) as accounts_count
+from accounts
+where income<20000
 union all
-select 'Average Salary' as category
-,count(case when income between 20000 and 50000 then 1 end) as accounts_count 
-from accounts 
+select "Average Salary" as category,
+count(*) as accounts_count
+from accounts
+where income between 20000 and 50000
 union all
-select 'High Salary' as category
-,count(case when income> 50000 then 1 end) as accounts_count 
-from accounts;
+select 'High Salary' as category,
+count(*) as accounts_count
+from accounts
+where income>50000;
