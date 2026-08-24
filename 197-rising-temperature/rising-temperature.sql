5ee08a99b7d7768d858cbec903cb832ca0ac9dfb
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-24 11:41:47 +0530

    Time: 477 ms (75.11%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,6 @@
 -- Aman
 select w1.id
 from weather as w1
-join weather as w2 on  datediff(w1.recorddate,w2.recorddate) = 1 
-where w1.temperature>w2.temperature;
+join weather as w2 
+where w1.temperature>w2.temperature and
+datediff(w1.recorddate,w2.recorddate) = 1 ;
