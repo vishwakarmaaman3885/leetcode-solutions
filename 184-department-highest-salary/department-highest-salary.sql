c22575beae992738167cfad88cb5224f7fe13f4a
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-25 12:14:54 +0530

    Time: 729 ms (65.73%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,8 +1,7 @@
 # Aman
-select d.name as department, e.name as employee, e.salary as salary
+select department,employee,salary
+from (select d.name as department, e.name as employee, e.salary as salary,
+dense_rank() over(partition by d.name order by e.salary desc) as rnk
 from employee as e
-join department as d on e.departmentid = d.id
-where e.salary in(select max(salary)
-from employee as e
-where e.departmentid = d.id )
-
+join department as d on e.departmentid = d.id) a
+where rnk = 1;
