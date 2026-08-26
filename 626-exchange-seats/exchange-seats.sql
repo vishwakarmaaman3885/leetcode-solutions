9f5768f37b920319222ca368bbacc4b15dd504ab
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-26 10:52:06 +0530

    Time: 348 ms (81.91%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,9 +1,10 @@
 # Aman
 select 
-case
-when id%2=1 and id+1<=(select max(id) from seat) then id+1
-when id%2=0 then id-1
-else id
-end as id,student
+if(
+    id%2=1,
+    if(id=(select max(id) from seat),id,id+1),
+    id-1
+) as id,
+student
 from seat
 order by id;
