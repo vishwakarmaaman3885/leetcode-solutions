48f62417964f256e26782ed1bab5040b757ae4e1
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-26 09:48:40 +0530

    Time: 310 ms (82.60%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,10 +1,13 @@
 # Aman
-select requester_id as id, count(requester_id) as num
-from(select requester_id
-from requestaccepted
-union all
-select accepter_id
-from requestaccepted) as count
-group by id
+with friends as(
+    select requester_id
+    from requestaccepted
+    union all
+    select accepter_id
+    from requestaccepted
+)
+select requester_id as id, count(*) as num
+from friends
+group by requester_id
 order by num desc
 limit 1;
