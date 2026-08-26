086b5c1c877c7bfba632b1822b40fac415f4995c
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-26 11:36:01 +0530

    Time: 725 ms (71.17%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,5 +1,4 @@
 # Aman
 select product_id, year as first_year, quantity, price
-from (select *, dense_rank() over(partition by product_id order by year) as rnk
-from sales) as a
-where rnk = 1;
+from sales
+where (product_id, year) in (select product_id, min(year) from sales group by product_id)
