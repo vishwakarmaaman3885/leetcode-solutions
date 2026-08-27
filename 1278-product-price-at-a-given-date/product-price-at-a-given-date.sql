9985c513df0464d81ca02e932a9fb3c77f24aeab
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-27 10:07:24 +0530

    Time: 570 ms (41.90%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,18 @@
+# Aman
+select product_id, new_price as price
+from products
+where (product_id,change_date) in (
+    select product_id, max(change_date) 
+    from products 
+    where change_date <="2019-08-16"
+    group by product_id
+)
+union
+select distinct product_id, 10 as price
+from products
+where product_id not in
+(
+    select product_id
+    from products
+    where change_date <= "2019-08-16"
+)
