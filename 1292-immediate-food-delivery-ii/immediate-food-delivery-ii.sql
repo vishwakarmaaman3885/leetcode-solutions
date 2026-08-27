33320206b217e24353df58f65f98b5a42c21c7c4
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-27 12:15:14 +0530

    Time: 687 ms (57.20%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -2,8 +2,10 @@
 select round(sum(case when order_date = customer_pref_delivery_date then 1 else
  0 end )
 *100/count(distinct customer_id),2) as immediate_percentage
-from (
-    select *, row_number() over(partition by customer_id order by order_date) as rnk
+from delivery
+where (customer_id,order_date) in
+(
+    select customer_id, min(order_date)
     from delivery
-) as a
-where rnk = 1;
+    group by customer_id
+); 
