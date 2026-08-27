8dc3fece206541f0d74a7d409cd3e352bb84a8e1
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-27 09:46:28 +0530

    Time: 1306 ms (67.39%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -1,8 +1,9 @@
 # Aman
-select u.user_id as buyer_id, u.join_date as join_date, 
-count(order_date) as orders_in_2019
+select u.user_id as buyer_id, u.join_date as join_date,
+count(
+    case when year(o.order_date) = 2019 then 1 
+    end
+) as orders_in_2019
 from users as u
-left join orders as o on u.user_id = o.buyer_id and
-year(o.order_date) = '2019'
-group by user_id
-
+left join orders as o on u.user_id = o.buyer_id
+group by u.user_id;
