7754535ac0022171bdcc750e82e520d051141221
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-27 11:42:12 +0530

    Time: 514 ms (61.53%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,11 +1,19 @@
 # Aman
 select product_id, new_price as price
-from(select *, row_number() over(partition by product_id order by change_date desc) as rnk
 from products
-where change_date<='2019-08-16') as a
-where rnk = 1
-union 
-select product_id, 10 as price
+where (product_id,change_date) in (
+    select product_id, max(change_date)
+    from products
+    where change_date <='2019-08-16'
+    group by product_id
+)
+union
+select distinct product_id, 10 as price
 from products
-group by product_id
-having min(change_date) > '2019-08-16';
+where product_id not in
+(
+    select product_id
+    from products
+    where change_date <='2019-08-16'
+   
+)
