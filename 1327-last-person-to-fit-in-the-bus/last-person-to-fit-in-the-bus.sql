305fbeff0a4c4fe2b3558665820da875a5d8181d
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-27 13:00:35 +0530

    Time: 2314 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,8 +1,8 @@
 # Aman
-select person_name
-from(
-    select *,sum(weight) over(order by turn) as total_weight
-    from queue ) a
-where  total_weight <=1000
-order by total_weight desc
+select q1.person_name as person_name
+from queue as q1
+join queue as q2 on q1.turn>=q2.turn
+group by q1.turn, q1.person_name
+having sum(q2.weight)<=1000
+order by q1.turn desc
 limit 1;
