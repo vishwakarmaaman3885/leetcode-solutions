d1573f0b15e31bb388d56e3f7e5c7fad20bf04e4
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-27 12:01:58 +0530

    Time: 713 ms (47.03%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,11 @@
+# Aman
+select round(sum(
+    case when order_date = customer_pref_delivery_date then 1 else 0 end
+)*100.0/count(distinct customer_id),2) as immediate_percentage
+from delivery
+where (customer_id,order_date) in
+(
+    select customer_id, min(order_date)
+    from delivery
+    group by customer_id
+)
