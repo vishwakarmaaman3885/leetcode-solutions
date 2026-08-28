401f01910d9ca0d2da440fed351acd6698b74b92
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-28 08:40:33 +0530

    Time: 317 ms (99.40%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,13 +1,11 @@
 # Aman
-select visited_on, amount, round((amount/7),2) as average_amount
-from( select visited_on,
-sum(daily_amount) 
-over(order by visited_on  rows between 6 preceding and current row) as amount, row_number() over(order by visited_on ) as rn
+select visited_on, amount, round(amount/7,2) as average_amount
+from( select visited_on, sum(daily_amount)
+over(order by visited_on rows between 6 preceding and current row) as amount,
+row_number() over(order by visited_on) as rn
 from(
     select visited_on, sum(amount) as daily_amount
     from customer
     group by visited_on
-)d)t
+) a) t
 where rn>=7;
-
-
