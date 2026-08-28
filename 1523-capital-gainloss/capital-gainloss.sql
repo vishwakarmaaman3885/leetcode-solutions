8851443ec1043b750ee1391756b454613ae26f03
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-28 09:57:59 +0530

    Time: 434 ms (99.88%) | Memory: 0B (100.00%) - LeetSync

--- 1523-capital-gainloss/capital-gainloss.sql
+++ 1523-capital-gainloss/capital-gainloss.sql
@@ -1,5 +1,5 @@
 # Aman
 select stock_name,
-sum(case when operation = "Sell" then price else-price end) as capital_gain_loss
+sum(if(operation = "Sell",price,-price)) as capital_gain_loss
 from stocks
 group by stock_name;
