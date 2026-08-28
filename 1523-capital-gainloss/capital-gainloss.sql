32db5b95421eeba57fbe54f7902d7d16e5222ed0
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-28 09:54:41 +0530

    Time: 524 ms (74.04%) | Memory: 0B (100.00%) - LeetSync

--- 1523-capital-gainloss/capital-gainloss.sql
+++ 1523-capital-gainloss/capital-gainloss.sql
@@ -1,5 +1,12 @@
 # Aman
-select stock_name,
-sum(if(operation = "sell",price,-price)) as capital_gain_loss
+with cte as (select stock_name, sum(price) as gained
 from stocks
-group by stock_name;
+where operation = "Sell"
+group by stock_name),
+ct as( select stock_name, sum(price) as spent
+from stocks
+where operation = "Buy"
+group by stock_name) 
+select a.stock_name, gained-spent as capital_gain_loss
+from cte as a
+join ct as b on a.stock_name = b.stock_name;
