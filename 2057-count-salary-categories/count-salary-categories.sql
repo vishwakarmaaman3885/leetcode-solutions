aa60086dd09ac01e3f0bcff720c5ebf4d53e41eb
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-28 10:07:58 +0530

    Time: 1602 ms (53.88%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,14 +1,13 @@
-select 'Low Salary' as category
-,count(*) as accounts_count
+# Aman
+select 'Low Salary' as category,
+count(*) as ACCOUNTS_COUNT
 from accounts
 where income<20000
 union all
-select "Average Salary" as category,
-count(*) as accounts_count
+select "Average Salary" as category, count(*) as accounts_count
 from accounts
 where income between 20000 and 50000
 union all
-select 'High Salary' as category,
-count(*) as accounts_count
+select "High Salary" as category, count(*) as accounts_count
 from accounts
 where income>50000;
