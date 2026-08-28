f30f7341d98b82f9fe03dacc1e4d469d3dad5dd4
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-28 09:24:41 +0530

    Time: 1342 ms (75.87%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,44 +1,20 @@
-# Aman
-WITH user_ratings AS (
-    SELECT
-        u.name,
-        COUNT(*) AS rating_count
-    FROM Users u
-    JOIN MovieRating r
-        ON u.user_id = r.user_id
-    GROUP BY u.user_id, u.name
-),
-
-top_user AS (
-    SELECT name
-    FROM user_ratings
-    ORDER BY rating_count DESC, name ASC
-    LIMIT 1
-),
-
-movie_ratings AS (
-    SELECT
-        m.title,
-        AVG(r.rating) AS avg_rating
-    FROM Movies m
-    JOIN MovieRating r
-        ON m.movie_id = r.movie_id
-    WHERE r.created_at >= '2020-02-01'
-      AND r.created_at < '2020-03-01'
-    GROUP BY m.movie_id, m.title
-),
-
-top_movie AS (
-    SELECT title
-    FROM movie_ratings
-    ORDER BY avg_rating DESC, title ASC
-    LIMIT 1
-)
-
-SELECT name AS results
-FROM top_user
-
-UNION ALL
-
-SELECT title AS results
-FROM top_movie;
+# 
+with cte as(select  u.name as title, count(*) as rating_count
+from users as u join movierating as r on u.user_id = r.user_id 
+group by u.user_id, u.name),
+top_user as( select title
+from cte 
+order by rating_count desc,title asc
+limit 1),
+movie_rating as ( select m.title as title, avg(r.rating) as avg_rating
+from movies as m join movierating as r on m.movie_id = r.movie_id
+where r.created_at between "2020-02-01" and "2020-02-29"
+group by m.movie_id, m.title),
+top_movies as (select title from movie_rating 
+order by avg_rating desc, title asc
+limit 1) 
+select title as results
+from top_user
+union all
+select title as results
+from top_movies;
