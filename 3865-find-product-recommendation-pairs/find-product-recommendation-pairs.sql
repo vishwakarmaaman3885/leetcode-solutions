8901ce689fbfb9521afa33fb3a9419e4d443456c
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-29 11:54:50 +0530

    Time: 632 ms (75.09%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3865-find-product-recommendation-pairs/find-product-recommendation-pairs.sql
@@ -0,0 +1,14 @@
+# Aman
+select p1.product_id as product1_id,
+p2.product_id as product2_id,
+i1.category as product1_category,
+i2.category as product2_category,
+count(p1.user_id) as customer_count
+from productpurchases as p1
+join productpurchases as p2 on p1.user_id =  p2.user_id 
+and p1.product_id<p2.product_id
+join productinfo as i1 on p1.product_id = i1.product_id
+join productinfo as i2 on p2.product_id = i2.product_id
+group by product1_id,product2_id,product1_category,product2_category
+having count(distinct p1.user_id)>=3
+order by customer_count desc,product1_id asc,product2_id asc;
