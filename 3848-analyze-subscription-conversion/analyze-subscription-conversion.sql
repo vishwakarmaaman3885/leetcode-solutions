302ca448172e76ee744c9be7216da124d10e9f2c
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-29 11:24:06 +0530

    Time: 338 ms (67.87%) | Memory: 0B (100.00%) - LeetSync

--- 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
+++ 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
@@ -1,5 +1,5 @@
 # Aman
-select user_id,
+with ct as (select user_id,
 round(avg(if(activity_type = "free_trial", activity_duration,Null)),2) as
 trial_avg_duration,
 round(avg(if(activity_type = "paid",activity_duration,null)),2) as
@@ -7,4 +7,6 @@ paid_avg_duration
 from useractivity
 group by user_id
 having trial_avg_duration is not null and  paid_avg_duration is not null
-order by user_id;
+order by user_id)
+select *
+from ct;
