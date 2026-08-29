c817667fda322b6ade279ad3dae9b56ba6cf1ce7
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-29 08:24:49 +0530

    Time: 667 ms (81.11%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,6 +1,6 @@
 # Aman
 select s.user_id,
-ifnull(round(sum(c.action = 'Confirmed')/count(*),2),0) as confirmation_rate
+round(ifnull(sum(c.action = "Confirmed")/count(*),0),2) as confirmation_rate
 from signups as s
 left join confirmations as c on s.user_id = c.user_id
 group by s.user_id;
