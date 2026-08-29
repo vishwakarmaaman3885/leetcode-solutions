5c1b5b244ea636c8d37d66a082556f041df58ec1
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-08-29 09:08:41 +0530

    Time: 315 ms (64.92%) | Memory: 0B (100.00%) - LeetSync

--- 3767-find-students-who-improved/find-students-who-improved.sql
+++ 3767-find-students-who-improved/find-students-who-improved.sql
@@ -1,8 +1,8 @@
 # Aman
-with exam_score as
-(select student_id, subject,
-first_value(score) over(partition by student_id,subject order by exam_date asc)as first_score,
-first_value(score) over(partition by student_id,subject order by exam_date desc) as latest_score
+with exam_score as ( select student_id, subject, 
+first_value(score) over(partition by student_id, subject order by exam_date) as first_score,
+first_value(SCORE) over(partition by student_id, subject order by exam_date desc ) as
+latest_score
 from scores)
 select distinct *
 from exam_score
