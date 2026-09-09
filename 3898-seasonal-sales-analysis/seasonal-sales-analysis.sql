81e4214562008564bfe8f685f0c1d41c4d390b14
Aman Kumar Vishwakarma <vishwakarmaaman3885@gmail.com>
2026-09-09 19:08:36 +0530

    Time: 535 ms (83.87%) | Memory: 0B (100.00%) - LeetSync

--- 3898-seasonal-sales-analysis/seasonal-sales-analysis.sql
+++ 3898-seasonal-sales-analysis/seasonal-sales-analysis.sql
@@ -1,16 +1,20 @@
 # Aman
-select season,category,total_quantity,total_revenue
-from 
-(select case 
-when month(sale_date) between 3 and 5 then  "Spring"
-when month(sale_date) between 6 and 8 then  "Summer"
-when month(sale_date) between 9 and 11 then "Fall" 
-else 'Winter' end as season, category,
-sum(quantity) as total_quantity ,
-sum(quantity*price) as total_revenue
-from sales as s 
-join products as p on s.product_id = p.product_id
-group by season,category
-order by total_quantity desc, total_revenue desc) as a
-group by season
-order by season;
+with cte as(
+    select case
+    when month(sale_date) in(12,1,2) then 'Winter'
+    when month(sale_date) in(3,4,5) then 'Spring'
+    when month(sale_date) in(6,7,8) then 'Summer'
+    else "Fall" end as Season, category,
+    sum(quantity) as total_quantity,
+    sum(price*quantity) as total_revenue
+    from sales as s 
+    join products as p on s.product_id = p.product_id
+    group by season, category
+)
+,ranked_categories as(
+    select *, dense_rank() over(partition by season order by total_quantity desc,  total_revenue desc) as rnk
+    from cte)
+    select season, category, total_quantity, total_revenue
+    from ranked_categories
+    where rnk = 1
+    order by season 
